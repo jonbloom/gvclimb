eba06ffdeee134e99c0e30554d892fee50f2c4c1
Jon Bloom <jon@Jons-MacBook-Pro-2.local>
2014-01-21 15:45:00 -0500

    commit

--- gvclimb.py
+++ gvclimb.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
 abort, render_template, flash
@@ -26,9 +28,54 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_all_routes():
-	cur = g.db.execute('select * from routes order by rating asc,rope')
-	routes_toprope = [dict(id=row[0], routeType=row[1], rating=row[2], rope=row[3], name=row[4], dateSet=row[5], setter=row[6]) for row in cur.fetchall() if row[1] == 'toprope']
-	routes_boulder = [dict(id=row[0], routeType=row[1], rating=row[2], rope=row[3], name=row[4], dateSet=row[5], setter=row[6]) for row in cur.fetchall() if row[1] == 'boulder']
-	return render_template('show_routes.html', routes_toprope=routes_toprope, routes_boulder=routes_boulder)
+	all_routes = g.db.execute('select * from routes AS r JOIN orderkeys AS k on k.rating = r.rating order by k.key')
+	default_colors = g.db.execute('select * from colors')
+	routes_toprope = []
+	routes_boulder = []
+	for row in all_routes:
+		# if row[9] == "none":
+		# 	base = default_colors[row[2]]
+		# else:
+		# 	base = row[9]
+		if row[1] == "toprope":
+			routes_toprope.append(dict(id=row[0], routeType=row[1], rating=row[2], rope=row[3], name=row[4], dateSet=row[5], setter=row[6],tape_div=gen_tape_div("white",row[7],row[8])))
+		else:
+			routes_boulder.append(dict(id=row[0], routeType=row[1], rating=row[2], rope=row[3], name=row[4], dateSet=row[5], setter=row[6],tape_div=gen_tape_div("white",row[7],row[8])))
+	return render_template('show_routes.html', topropes=routes_toprope, boulders=routes_boulder)
+def gen_tape_div(base_color,color_1,color_2):
+	tape_div = "<div class='color " + base_color +  "' style='background-color:" + base_color + "; width:auto; \height:20px;margin:0 auto; padding:10px'>"
+      	if color_1 != 'none':
+      		if color_2 == 'none':
+      			tape_div =  tape_div + "<div class='color " + color_1 + " fulltape' style='background-color:" + color_1 + "; width:40%; height:20px; margin:0 auto;'></div>"
+      		else:
+      			tape_div = tape_div +  "<div class='color " + color_1 + " halftape' style='background-color:" + color_1 + "; width:40%; height:20px; margin:0 5%; float:left;'></div><div class='color " + color_2 + " halftape' style='background-color:" + color_2 + "; width:40%; height:20px; margin:0 5%; float:left;'></div>"
+      	tape_div = tape_div + "</div>"
+      	return tape_div
+# class Route(object):
+# 	id = 0
+# 	route_type = ""
+# 	rating = ""
+# 	rope = 0
+# 	name = ""
+# 	date_set = ""
+# 	setter = ""
+# 	base_color = "none"
+# 	color_1 = "none"
+# 	color_2 = "none"
+# 	special_base = "none"
+# 	def __init__(self,route_type,rating,rope,name,date_set,setter,base_color,color_1,color_2,special_base):
+# 		self.route_type = route_type
+# 		self.rating = rating
+# 		self.rope = rope
+# 		self.name = name
+# 		self.date_set = date_set
+# 		self.setter = setter
+# 		self.base_color = base_color
+# 		self.color_1 = color_1
+# 		self.color_2 = color_2
+# 		self.special_base = special_base
+
+
+
 if __name__ == '__main__':
 		app.run()
--- schema.sql
+++ schema.sql
@@ -14,3 +14,30 @@ create table routes (
   blurb text,
   updater text 
 );
+create table colors(
+  [7] text,
+  [8] text,
+  [9] text,
+  [10a] text,
+  [10b] text,
+  [10c] text,
+  [10d] text,
+  [11a] text,
+  [11b] text,
+  [11c] text,
+  [11d] text,
+  [12a] text,
+  [12b] text,
+  [12c] text,
+  [12d] text,
+  [V0] text,
+  [V1] text,
+  [V2] text,
+  [V3] text,
+  [V4] text,
+  [V5] text,
+  [V6] text,
+  [V7] text,
+  [V8] text,
+  [V9] text
+);
