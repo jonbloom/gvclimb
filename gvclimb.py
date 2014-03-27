bbd972a93bc60dbf38ec03dc9139650f4cab640a
Jon Bloom <github@jon.tw>
2014-03-27 08:24:34 -0400

    whoops

--- gvclimb.py
+++ gvclimb.py
@@ -4,7 +4,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
 abort, render_template, flash
 
 # configuration
-DATABASE = '/var/www/gvclimb/gvclimb.sqlite'
+DATABASE = '/var/www/gvclimb.sqlite'
 DEBUG = True
 SECRET_KEY = 'secret'
 USERNAME = 'jonbloom'
@@ -20,8 +20,6 @@ def before_request():
 	g.db = connect_db()
 	g.db.row_factory = sqlite3.Row
 	g.data = get_data()
-	if 'logged_in' in session and session['logged_in']:
-		g.username = session['logged_in_as']
 @app.teardown_request
 def teardown_request(exception):
 	db = getattr(g, 'db', None)
@@ -37,9 +35,9 @@ def get_data():
 	attempted_ids = []
 	user_voted_ids = []
 	if 'logged_in' in session and session['logged_in']:
-		attempted_ids = g.db.execute('select route_id from attempts where username = ?', [g.username]).fetchall()
-		sent_ids = g.db.execute('select route_id from sends where username = ?', [g.username]).fetchall()
-		user_voted_ids = g.db.execute('select route_id from votes where username = ?', [g.username]).fetchall()
+		attempted_ids = g.db.execute('select route_id from attempts where username = ?', [session['logged_in_as']]).fetchall()
+		sent_ids = g.db.execute('select route_id from sends where username = ?', [session['logged_in_as']]).fetchall()
+		user_voted_ids = g.db.execute('select route_id from votes where username = ?', [session['logged_in_as']]).fetchall()
 		attempted_ids = [int(i[0]) for i in attempted_ids if i]
 		sent_ids = [int(i[0]) for i in sent_ids if i]
 		user_voted_ids = [int(i[0]) for i in user_voted_ids if i]
@@ -242,6 +240,8 @@ def comm_reset(route_id):
 def update_colors():
 	require_logged_in()
 	require_admin()
+	default_colors = g.db.execute('select * from colors')
+	default_colors = dict(default_colors.fetchone())
 	c_V0 = request.form['V0']
 	c_V1 = request.form['V1']
 	c_V2 = request.form['V2']
@@ -373,19 +373,13 @@ def logout():
     session.pop('is_admin', None)
     return redirect(url_for('home'))
 
-@app.errorhandler(400)
-def custom_400(error):
-	message = "There's really no reason for you to see this page."
-	return render_template('error.html',error=error,message=message,code=400), 400
-
 @app.errorhandler(401)
 def custom_401(error):
 	message = "You are attempting to access a restricted area."
 	return render_template('error.html',error=error,message=message,code=401), 401
-
 @app.errorhandler(404)
 def custom_404(error):
-	message = "Whatever you were looking for does not exist."
+	message = "WHatever you were looking for does not exist."
 	return render_template('error.html',error=error, message=message,code=404), 404
 @app.errorhandler(405)
 def custom_405(error):
