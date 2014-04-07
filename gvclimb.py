e7ebc38fd1bf06f86744074e5202b94bd6eed6c2
Jon Bloom <github@jon.tw>
2014-04-07 12:59:32 -0400

    css fun

--- gvclimb.py
+++ gvclimb.py
@@ -4,7 +4,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
 abort, render_template, flash
 
 # configuration
-DATABASE = '/var/www/gvclimb/gvclimb.sqlite'
+DATABASE = 'gvclimb.sqlite'
 DEBUG = True
 SECRET_KEY = 'secret'
 USERNAME = 'jonbloom'
